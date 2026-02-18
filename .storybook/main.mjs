00e67660b2d09c2945443d5b027f540972522500
unadlib <unadlib@gmail.com>
2026-02-19 02:07:00 +0800

    chore(deps): upgrade runtime and dev dependencies

--- .storybook/main.mjs
+++ .storybook/main.mjs
@@ -1,7 +1,7 @@
 /** @type {import('@storybook/html-vite').StorybookConfig} */
 const config = {
   stories: ['../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
+  addons: ['@storybook/addon-a11y'],
   framework: {
     name: '@storybook/html-vite',
     options: {},
