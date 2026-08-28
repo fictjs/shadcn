a76e66bcae1e1a7f30103b2f03e2ba30a19f7f7c
unadlib <unadlib@gmail.com>
2026-08-29 03:02:34 +0800

    fix(website): align Aspect Ratio previews with Fict sources

---
+++ apps/v4/content/examples/fict/aspect-ratio/aspect-ratio-square.tsx
@@ -0,0 +1,7 @@
+import { AspectRatio } from '@/components/ui/aspect-ratio'
+
+export default function AspectRatioSquareExample() {
+  return (
+    <AspectRatio ratio={1}><img class="h-full w-full rounded-md object-cover" src="https://images.unsplash.com/photo-1500530855697-b586d89ba3ee" alt="Landscape" /></AspectRatio>
+  )
+}
