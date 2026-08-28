6ea933ad43348521eb52cfa2086a33b282ee997d
unadlib <unadlib@gmail.com>
2026-08-29 03:02:36 +0800

    fix(website): align Input Otp previews with Fict sources

---
+++ apps/v4/content/examples/fict/input-otp/input-otp-invalid.tsx
@@ -0,0 +1,7 @@
+import { InputOTP, InputOTPGroup, InputOTPSeparator, InputOTPSlot } from '@/components/ui/input-otp'
+
+export default function InputOTPInvalidExample() {
+  return (
+    <InputOTP><InputOTPGroup>{[0, 1, 2].map(index => <InputOTPSlot index={index} total={6} />)}</InputOTPGroup><InputOTPSeparator /><InputOTPGroup>{[3, 4, 5].map(index => <InputOTPSlot index={index} total={6} />)}</InputOTPGroup></InputOTP>
+  )
+}
